bcf038004aa818b996d197516873488cd9d76be9
tom <tom.schlenker@gmx.de>
2024-06-21 14:49:40 +0200

    add lcone format transformation to .npz

--- mcmc.py
+++ mcmc.py
@@ -3,8 +3,6 @@ import emcee
 from pymultinest.solve import solve
 import json
 os.environ["OMP_NUM_THREADS"] = "1"
-from multiprocessing import set_start_method
-set_start_method("spawn")
 from schwimmbad import MPIPool
 
       
@@ -120,65 +118,6 @@ class mcmc(Simulation):
             not_sane = np.isnan(data.brightness_temp).any()
             print(f"{not_sane=}")
         return data
-
-    @staticmethod
-    def convert_to_npz(path: str, prefix: str = "", check_for_nan: bool = True, debug: bool = False) -> None:
-        '''Given a path and an optinal prefix 
-        (e.g. only convert all files named as run_, set prefix = "run_")
-        this function converts .h5 files from 21cmfastwrapper to the common .npz format'''
-        # image, label, tau, gxH
-        # search for all files given in a path given a prefix an loop over those
-        files = fnmatch.filter(os.listdir(path), prefix + "*")
-        if debug: print(f"{files}")
-        len_files = len(files)
-        nan_counter = []
-        # initalize the progress bar
-        
-        for i,file in enumerate(files):
-            lcone = p21c.outputs.LightCone.read(file)
-            # load image
-            image = lcone.brightness_temp
-            # check if there are NaNs in the brightness map
-            if check_for_nan:
-                if np.isnan(image).any():
-                    nan_counter.append(file)
-                    continue
-            #load labels, WDM,OMm,LX,E0,Tvir,Zeta
-            labels = [
-                lcone.global_params["M_WDM"],
-                lcone.cosmo_params.OMm,
-                lcone.astro_params.L_X,
-                lcone.astro_params.NU_X_THRESH,
-                lcone.astro_params.ION_Tvir_MIN,
-                lcone.astro_params.HII_EFF_FACTOR
-            ]
-            # load redshift
-            redshifts = lcone.node_redshifts
-            # compute tau
-            gxH=lcone.global_xH
-            gxH=gxH[::-1]
-            redshifts=redshifts[::-1]
-            tau=p21c.compute_tau(redshifts=redshifts,global_xHI=gxH)
-
-            new_format = {
-                "image": image,
-                "label": labels,
-                "tau": tau,
-                "z": redshifts,
-                "gxH": gxH
-            }
-            #save to new format
-            np.savez(file + ".npz", **new_format)
-            
-            # progress counter
-            if len_files >= 10:
-                if i % (int(len_files/10)) == 0:
-                    idx = int(i / int(len_files) * 10)
-                    progress = "-"*(idx) + ">" + (10-idx)*" "
-                    print(f"|{progress}|", end='', flush=True)
-                    print("\r", end='')
-        print(f"Done, {len(nan_counter)} NaNs encountered in \n{nan_counter}")    
-
     @staticmethod
     def nan_adversarial(bt_cone):
         nans = np.isnan(bt_cone)
